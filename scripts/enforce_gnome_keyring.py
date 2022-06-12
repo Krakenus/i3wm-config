0ad78afec9a1647f840a80bb7abf7117c400eb15
Milan Vlasák <krakenus02@gmail.com>
2022-06-12 22:26:13 +0200

    using argparse

--- scripts/enforce_gnome_keyring.py
+++ scripts/enforce_gnome_keyring.py
@@ -1,8 +1,8 @@
-import sys
 import re
 import os
 import shutil
 import subprocess
+import argparse
 
 regex = re.compile(r'^Exec=([\w/-]+)(:? +(.*))?$')
 
@@ -14,11 +14,13 @@ def copy_file(source: str, target: str, stat_source: bool = False):
     os.chown(target, stat.st_uid, stat.st_gid)
 
 
-def process_file(file_path: str):
+def make_backup(file_path: str):
+    copy_file(file_path, f'{file_path}.backup', stat_source=True)
+
+
+def _process_file(file_path: str):
     file_name = os.path.basename(file_path)
     tmp_path = os.path.join('/', 'tmp', file_name)
-    
-    copy_file(file_path, f'{file_path}.backup', stat_source=True)
     with open(file_path) as infile, open(tmp_path, 'w') as outfile:
         for line in infile:
             re_match = regex.match(line)
@@ -32,21 +34,36 @@ def process_file(file_path: str):
     copy_file(tmp_path, file_path)
 
 
+def process_file(args: argparse.Namespace, file_path: str):
+    if not args.no_backup:
+        make_backup(file_path)
+    _process_file(file_path)
+
+
 def get_file_locations(app_name: str):
     proc = subprocess.Popen(['locate', app_name], stdout=subprocess.PIPE)
     while line := proc.stdout.readline():
         yield line.decode().strip()
 
 
-def main():
-    apps = sys.argv[1:]
-    for app_name in apps:
+def main(args: argparse.Namespace):
+    for app_name in args.apps:
         if not app_name.endswith('.desktop'):
             app_name = f'{app_name}.desktop'
+        print(f'Processing {app_name}...')
         for path in get_file_locations(app_name):
-            print(path)
-            process_file(path)
+            print(f' - {path}')
+            process_file(args, path)
 
+
+def add_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, help='Path to config file')
+    parser.add_argument('--no-backup', action='store_true', dest='no_backup', help='Do not create backup files')
+    parser.add_argument('-a', '--apps', type=str, nargs='*', help='List of applications to be processed (ignored when using -c --config)')
+    return parser
 
 if __name__ == '__main__':
-    main()
+    parser = add_arguments()
+    args = parser.parse_args()
+    main(args)
